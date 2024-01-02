304e66d252013b7442c1f958dee24814edec24e4
Tin Novakovic <tin.novakovic@comparethemarket.com>
2024-01-02 23:11:25 +0000

    Moving classes to different package

--- app/src/main/java/com/tinnovakovic/videostreamer/remote/StreamerApi.kt
+++
@@ -1,9 +0,0 @@
-package com.tinnovakovic.videostreamer.remote
-
-import com.tinnovakovic.videostreamer.data.models.Lesson
-import com.tinnovakovic.videostreamer.data.models.Subject
-
-interface StreamerApi {
-    fun getSubjects(): List<Subject>
-    fun getLessons(): List<Lesson>
-}
